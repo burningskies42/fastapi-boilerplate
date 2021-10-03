1b197eb30c0739556479086b139d7986b976ee43
Leon <burningskies42@gmail.com>
2021-10-03 11:52:49 +0200

    added docstrings

--- app/main.py
+++ app/main.py
@@ -12,17 +12,19 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+# routes for non-static pages
 app.include_router(unsplash.router)
 app.include_router(twoforms.router)
 app.include_router(accordion.router)
 
-
+# home page
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     data = open_file("home.md")
     return templates.TemplateResponse("page.html", {"request": request, "data": data})
 
 
+# load other static pages
 @app.get("/page/{page_name}", response_class=HTMLResponse)
 async def page(request: Request, page_name: str):
     data = open_file(page_name + ".md")
